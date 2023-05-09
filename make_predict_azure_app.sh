89a99570f4a128c9d43be22aa28507a4031cb3e0
gra2378 <133028180+gra2378@users.noreply.github.com>
2023-05-09 17:21:07 -0500

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flask-ml-serv-cy.azurewebsites.net:$PORT/predict 
+     -X POST https://testdbdd.azurewebsites.net/:$PORT/predict 
      #your application name <yourappname>goes here
